fbcad7117a7ab9f5ae251c4d9cfa729465dc55a4
Rosaura515 <roramirez.phyc23@bootcamp.ucamp.io>
2024-07-14 23:04:34 -0600

    C:\Users\roxya\modulo2-M\Reto_semana06.py

---
+++ Reto_semana06.py
@@ -0,0 +1,12 @@
+#Crear un programa que pida la contraseña,la verifique y salga del programa en caso de error.
+entrada=input("Hola! introduce tu contraseña: ") 
+numero=int=(6)
+entrada2=input("escribe nuevamente tu contraseña: ")
+numero2=int=(6)
+random=numero_de_intentos=3
+numero_de_intentos>0
+resultado=("intentos restantes:" ,numero_de_intentos )
+int=entrada==entrada2
+thrue= ("Bienvenido: ")
+False 
+print_return =("Programa cerrado por seguridad,intentar nuevamente dentro de 5 minutos":)
