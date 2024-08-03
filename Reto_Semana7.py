13be5e440d086cf6d7f5d3c7543a098a360d7c13
Rosaura Ramírez Segovia <roramirez.phyc23@bootcamp.ucamp.io>
2024-08-03 04:54:10 -0600

    Create Reto_Semana7.py

---
+++ Reto_Semana7.py
@@ -0,0 +1,16 @@
+#Reto_Semana7.py
+from os import system
+list ()
+opcion = input("escribe el nombre del alumno (1) ")
+opcion = input("salir del programa (2) ")
+nombre = input("escribe el nombre del alumno: ").capitalize()
+nota1 = int(input(f"ingresar primera nota de {nombre}: "))
+nota2 = int(input(f"ingresar segunda nota de {nombre}: "))
+nota3 = int(input(f"ingresar tercera nota de {nombre}: "))
+float  = nota1,nota2,nota3 
+promedio=(nota1+nota2+nota3)/3
+print("El promedio de las 3 notas es: " == promedio)
+if opcion ==2:
+   print (input ( "El programa ha terminado :"))
+
+
